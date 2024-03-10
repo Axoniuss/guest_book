ca332ce475d1b2cd54dd9e9a6a8a4d183e2512d7
Axoniuss <klim.vlad@list.ru>
2024-03-10 12:26:00 +0300

    guest book with connect db

--- helpers/funcs.php
+++ helpers/funcs.php
@@ -1,9 +1,35 @@
 <!--файл funcs.php - функция save_mess() -->
 
 <?php
+// save_mess для записи журнала в файл с проверкой в поле text
 function save_mess(){
-    $str = $_POST['name'] . '|' . $_POST['text'] . '|' . date('Y-m-d H:i:s') . "\n***\n" ;
+
+    if (!empty($_POST['text']))
+    {
+        $str = $_POST['name'] . '|' . $_POST['text'] . '|' . date('Y-m-d H:i:s') . "\n***\n" ;
 // записываем все в файл gb.txt с помощью функции file_put_contents
+    }
     file_put_contents('gb.txt', $str, FILE_APPEND);
 }
+// Функция берет все данные из док gb.txt
+function get_mess(){
+
+    return file_get_contents('gb.txt');
+}
+//каждый разделитель служит на новый элемент массива, где найдет ***
+// Вмещает все данные из gb.txt использует разделитель
+function array_mess($messages){
+    $messages = explode("\n***\n" , $messages);
+    array_pop($messages);
+    return $messages ;
+}
+function get_format_message($message){
+    return explode('|', $message);
+}
+
+// Печатает в удобный формат
+function print_arr($arr){
+    echo '<pre>' . print_r($arr , true) . '</pre>';
+}
 ?>
+
--- index.php
+++ index.php
@@ -1,44 +1,89 @@
+<!--get_mess = takes all the data from the file-->
+<!--array_mess = each separator is used for a new element of the array, where it will find ***-->
 <?php
 header("Content-type: text/html; charset=utf-8");
 error_reporting(-1);
+
+
+// Подключение к базе данных
+$db = @mysqli_connect('127.0.0.1', 'root', '', 'gbook') or die ('Ошибка соединения с БД');
+echo mysqli_connect_error();
+// если нет подключения, тогда мы выведем текст ошибки mysqli_connect_error(), и завершим дальнейшее выполнение скрипта - die
+if (!$db) die (mysqli_connect_error());
+var_dump($db); // смотрим, что получилось
+$insert = "INSERT INTO comments (login, text) VALUES ('Оля', 'ПРИВЕТ')";
+// выполняем запрос
+$res_insert = mysqli_query($db, $insert);
+
+// проверяем выполнение запроса
+if ($res_insert) echo 'Ok';
+else echo 'Error';
+
+// выводим строку с описанием последней ошибки (если она есть)
+echo mysqli_error($db);
+
+// второй способ посмотреть ошибку - вывести сам запрос
+echo $insert;
+
 require_once dirname(__DIR__) . '/guest_book/config/init.php';
 require_once HELPERS . "/funcs.php"; // подключаем файл funcs.php
 // принятие данных из формы
 // если у нас не пуст массив $_POST, то вызовем некую функцию: save_mess
-    print_r(("Location: {$_SERVER ['PHP_SELF' ]}"));
-if (! empty ( $_POST )){
+if (!empty ($_POST)) {
     save_mess();// пользовательская функция
     // редирект на этот же самый скрипт
-    header ("Location: {$_SERVER ['PHP_SELF' ]}");
+    header("Location: {$_SERVER ['PHP_SELF' ]}");
     exit;
 }
-
+echo 'Вывод массива';
+$messages = get_mess();
+$messages = array_mess($messages);
+//print_arr($messages);
 ?>
 
 
 <!doctype html>
 <html Lang="en">
 <head>
-    <meta charset ="UTF-8">
+    <meta charset="UTF-8">
     <title>Гостевая книга</title>
 </head>
 <body>
 <!-- Создаем форму -->
-<form action = "index.php" method = "post">
+<form action="index.php" method="post">
     <p>
-        <h1>Гостевая книга</h1>
-        <!-- название поля -->
-        <label for ="name"> Имя:</label>
-        <!-- поле для ввода имени -->
-        <input type ="text" name ="name" id ="name">
+    <h1>Гостевая книга</h1>
+    <!-- название поля -->
+    <label for="name"> Имя:</label>
+    <!-- поле для ввода имени -->
+    <input type="text" name="name" id="name">
     </p>
     <p>
         <!-- название поля -->
-        <label for ="text"> Текст:</label>
+        <label for="text"> Текст:</label>
         <!-- поле для ввода текста -->
-        <textarea name = "text" id ="text"></textarea>
+        <textarea name="text" id="text"></textarea>
     </p>
     <!-- кнопка -->
-    <button type ="submit"> Написать </button>
+    <button type="submit"> Написать</button>
 </form>
+
+<hr>
+
+<?php
+if (!empty ($messages)): ?>
+    <!-- проходим по массиву $messages в цикле и выводим наши сообщения -->
+    <?php foreach ($messages as $message) : ?>
+        <!-- возвращает, разбитую в массив строку -->
+        <?php $message = get_format_message($message); //print_r($message) ?>
+        <!-- выводим полученные наши данные -->
+        <div class="messages">
+            <hr>
+            <p> Автор: <?= $message[0] ?> | Дата: <?= $message[2] ?> </p>
+            <?= nl2br(htmlspecialchars($message[1])) ?>
+            <hr>
+        </div
+    <?php endforeach; ?>
+<?php endif ?>
 </body>
+</html>
