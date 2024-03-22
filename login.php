787208582b0d2a5f5b7d81b8320e53a7881775b4
Axoniuss <klim.vlad@list.ru>
2024-03-22 13:17:55 +0300

    css

--- login.php
+++ login.php
@@ -5,13 +5,14 @@
 <title>регистрация и авторизации пользователей</title>
 <link href="css/style.css" media="screen" rel="stylesheet">
 <link href= 'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!-- <link rel="stylesheet" href="styles.css"> -->
 </head> 
 <body>
 <div class="container mlogin">
 <div id="login">
 <h1>Вход</h1>
 <form action="" id="loginform" method="post"name="loginform">
-<p><label for="user_login">Имя опльзователя<br>
+<p><label for="user_login">Имя пользователя<br>
 <input class="input" id="username" name="username"size="20"
 type="text" value=""></label></p>
 <p><label for="user_pass">Пароль<br>
